b9661527a3c0be58fd9fca19a9ba935bb67157d1
Tweaka <vzxpk@yahoo.fr>
2022-04-27 22:33:28 +0200

    refactoring code

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,5 +1,6 @@
 import { json } from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
+import { getPosts } from "~/models/post.server";
 
 type Post = {
   slug: string;
@@ -7,21 +8,12 @@ type Post = {
 };
 
 type LoaderData = {
-  posts: Array<Post>;
+  posts: Awaited<ReturnType<typeof getPosts>>
 };
 
 export const loader = async () => {
   return json<LoaderData>({
-    posts: [
-      {
-        slug: "my-first-post",
-        title: "My First Post",
-      },
-      {
-        slug: "90s-mixtape",
-        title: "A Mixtape I Made Just For You",
-      },
-    ],
+    posts: await getPosts(),
   });
 };
 
